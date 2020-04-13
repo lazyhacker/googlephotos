964500214f40417ffd8fdc4cfafd249e4d51de58
LazyHacker <github@lazyhacker.com>
2020-04-13 14:10:28 -0700

    add a blank top package

---
+++ doc.go
@@ -0,0 +1,6 @@
+// package googlephotos is a fork of Google's Go client library for the [Google
+// Photos APIs](https://developers.google.com/photos) before it was
+// [removed](https://code-review.googlesource.com/c/google-api-go-client/+/39951)
+// from the [Google API Client
+// Libraries](https://developers.google.com/api-client-library).
+package googlephotos
